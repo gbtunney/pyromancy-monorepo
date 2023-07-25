07ba4f29a450373a4e8631ddf1ee3f7b915889a9
Gillian Tunney <gbtunney@mac.com>
2023-07-25 17:17:31 -0400

    build(root): gitignore fixes

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -3,5 +3,5 @@
 /** @file Local package prettier config file */
 const { Prettier, merge } = require('@snailicide/build-config')
 module.exports = merge(Prettier.config, {
-    plugins: ['@prettier/plugin-xml'],
+    plugins: ['prettier-plugin-xml'],
 })
