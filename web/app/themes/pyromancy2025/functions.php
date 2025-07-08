cb06535f5965712882dd578ad25bf496370c8bc2
Gillian Tunney <gbtunney@mac.com>
2025-07-08 01:50:50 -0400

    fix(theme-pyromancy2025): fixed vites ssl https issues

--- web/app/themes/pyromancy2025/functions.php
+++ web/app/themes/pyromancy2025/functions.php
@@ -2,52 +2,52 @@
 /**
  * Register/enqueue custom scripts and styles
  */
-add_action( 'wp_enqueue_scripts', function() {
-	// Enqueue your files on the canvas & frontend, not the builder panel. Otherwise custom CSS might affect builder)
-	if ( ! bricks_is_builder_main() ) {
-		wp_enqueue_style( 'bricks-child', get_stylesheet_uri(), ['bricks-frontend'], filemtime( get_stylesheet_directory() . '/style.css' ) );
-	}
-} );
+add_action('wp_enqueue_scripts', function () {
+    // Enqueue your files on the canvas & frontend, not the builder panel. Otherwise custom CSS might affect builder)
+    if (!bricks_is_builder_main()) {
+        wp_enqueue_style(
+            'bricks-child',
+            get_stylesheet_uri(),
+            ['bricks-frontend'],
+            filemtime(get_stylesheet_directory() . '/style.css')
+        );
+    }
+});
 require_once __DIR__ . '/vendor/autoload.php';
 use Kucrut\Vite;
 
-
-add_action( 'wp_enqueue_scripts', function (): void {
-	Vite\enqueue_asset(
-		__DIR__ . '/dist',
-		'src/index.ts',
-		[
-			'handle' => 'my-script-handle',
-			//'dependencies' => [ 'wp-components', 'some-registered-script-handle' ], // Optional script dependencies. Defaults to empty array.
-			///'css-dependencies' => [ 'wp-components', 'some-registered-style-handle' ], // Optional style dependencies. Defaults to empty array.
-			'css-media' => 'all', // Optional.
-			'css-only' => false, // Optional. Set to true to only load style assets in production mode.
-			'in-footer' => true, // Optional. Defaults to false.
-		]
-	);
-} );
-
+add_action('wp_enqueue_scripts', function (): void {
+    Vite\enqueue_asset(__DIR__ . '/dist', 'src/index.ts', [
+        'handle' => 'my-script-handle',
+        //'dependencies' => [ 'wp-components', 'some-registered-script-handle' ], // Optional script dependencies. Defaults to empty array.
+        ///'css-dependencies' => [ 'wp-components', 'some-registered-style-handle' ], // Optional style dependencies. Defaults to empty array.
+        'css-media' => 'all', // Optional.
+        'css-only' => false, // Optional. Set to true to only load style assets in production mode.
+        'in-footer' => true, // Optional. Defaults to false.
+    ]);
+});
 
 /**
  * Register custom elements
  */
-add_action( 'init', function() {
-  $element_files = [
-    __DIR__ . '/elements/title.php',
-  ];
+add_action(
+    'init',
+    function () {
+        $element_files = [__DIR__ . '/elements/title.php'];
 
-  foreach ( $element_files as $file ) {
-    \Bricks\Elements::register_element( $file );
-  }
-}, 11 );
+        foreach ($element_files as $file) {
+            \Bricks\Elements::register_element($file);
+        }
+    },
+    11
+);
 
 /**
  * Add text strings to builder
  */
-add_filter( 'bricks/builder/i18n', function( $i18n ) {
-  // For element category 'custom'
-  $i18n['custom'] = esc_html__( 'Custom', 'bricks' );
+add_filter('bricks/builder/i18n', function ($i18n) {
+    // For element category 'custom'
+    $i18n['custom'] = esc_html__('Custom', 'bricks');
 
-  return $i18n;
-} );
-
+    return $i18n;
+});
--- web/app/themes/pyromancy2025/vite.config.ts
+++ web/app/themes/pyromancy2025/vite.config.ts
@@ -1,33 +1,35 @@
-import { defineConfig } from 'vite'
-//import UnoCSS from 'unocss/vite'
-//import react from '@vitejs/plugin-react'
 import { v4wp } from '@kucrut/vite-for-wp'
+import { readFileSync } from 'fs'
+import { defineConfig,type ServerOptions} from 'vite'
+
+const server:ServerOptions = {
+  
+    host:  'pyromancy2025.lndo.site',
+    port: 5173,
+    https: {
+      key: readFileSync('./certs/local.key'),
+      cert: readFileSync('./certs/local.crt'),
+    },
+    cors: {
+      origin: 'https://pyromancy2025.lndo.site',
+    },
+  }
+
+
 
 export default defineConfig({
+server,
     plugins: [
-       v4wp({
-            input: 'src/index.ts',
-            outDir: 'dist'
-        }),
+      v4wp({
+        input: 'src/index.ts',
+        outDir: 'dist',
+      }),
       // UnoCSS(),
-       // react()
+      // react(),
     ],
-    server: {
-        host:true,
-        //host: "192.168.64.1",
-        port: 5173,
-        strictPort: true,
-        cors: {
-            origin: "http://pyromancy2025.lndo.site",
-            //credentials: true,
-        },
-       /* hmr: {
-            host: "192.168.64.1",
-            protocol: "ws",
-        },*/
-    },
+    
     build: {
-        emptyOutDir: true,
-        minify: false,
+      emptyOutDir: true,
+      minify: false,
     },
-})
+  })
